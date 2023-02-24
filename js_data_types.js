9a7abb2e9b457dd5819f57da76ddd8958214b007
MasudaAfrin <masudatuba@gmail.com>
2023-02-24 16:24:11 +0600

    primitive data type and falsy values

---
+++ js_data_types.js
@@ -0,0 +1,22 @@
+/*
+8 data types in js
+1. Number,
+2. String,
+3. Boolean,
+4. Undefined,
+5. Symbol,
+6. Object - object, array,
+7. Null,
+8. Bigint
+*/
+
+/*
+Falsy values in js
+
+1. False,
+2. NaN,
+3. Null,
+4. Undefined,
+5. ""
+6. 0 
+*/
